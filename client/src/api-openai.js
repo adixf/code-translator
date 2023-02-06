46791640d08102d6a5431de29f5ceaceb9d5fe9b
adixf <adixfuchs@gmail.com>
2023-02-06 12:57:36 +0200

    wip

--- client/src/api-openai.js
+++ client/src/api-openai.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-    apiKey: process.env.OPEMAI_KEY
+    apiKey: process.env.OPENAI_KEY
 });
 
 const openai = new OpenAIApi(configuration);
